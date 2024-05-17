7f8b489752003671167adcac08a9176dcbdc3cc5
Florian HULIN <flodmg@gmail.com>
2024-05-17 18:07:25 +0200

    Enum Manager Autority Description
    Change the description to 'component' to 'entity'

--- Source/Module_Basic/Public/Enum/ManagerAuthority.h
+++ Source/Module_Basic/Public/Enum/ManagerAuthority.h
@@ -9,22 +9,22 @@
 /**
 *  Manager authority :
 * State of the authority of the manager using this enum
-* Define how he will interact with the component he have the power on :
+* Define how he will interact with the entities linked to him he have the power on :
 *  - Will get the full power on them
-*  - Will controle them only if the component want to
-*  - Won't be able to controle any component
+*  - Will controle them only if the entities want to
+*  - Won't be able to controle any entities
 *
 */
 UENUM(BlueprintType)
 enum class EManagerAuthority : uint8
 {
-	/** The manager will haave full power on his linked component */
+	/** The manager will haave full power on his linked entity */
 	MANAGERAUTHORITY_Full = 0 UMETA(DisplayName = "Full"),
 
-	/** The manager will control only the component that chose to listen to him */
+	/** The manager will control only the entity that chose to listen to him */
 	MANAGERAUTHORITY_Listen UMETA(DisplayName = "Listen"),
 
-	/** The manager won't have any power on any component */
+	/** The manager won't have any power on any entity */
 	MANAGERAUTHORITY_Silence UMETA(DisplayName = "Silence"),
 
 	/** Size of this Enum */
